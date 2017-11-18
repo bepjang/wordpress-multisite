b112a3374adbb42d2bf4e11e80cadb3aee511bad
JUNGWON WOO <bepjang@gmail.com>
2017-11-18 10:22:01 +0900

    멀티사이트 생성

--- wp-config.php
+++ wp-config.php
@@ -82,7 +82,12 @@ define('WP_DEBUG', false);
 /* That's all, stop editing! Happy blogging. */
 /* Multisite */
 define( 'WP_ALLOW_MULTISITE', true );
-
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'memo01.cafe24.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
